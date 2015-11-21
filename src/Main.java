0e4ef85286fcd6af970c343eb4fc05741546dc4d
RioShine <tnnghia1994@gmail.com>
2015-11-21 10:25:08 +0700

    create class  main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Pkg_Add.Add;
+
+
+public class Main {
+	public static void main(String arg[])
+	{
+		Add add = new Add();
+		int result = add.Add(10, 20);
+		System.out.println(result);
+	}
+}
---
+++ src/Pkg_Add/Add.java
@@ -0,0 +1,8 @@
+package Pkg_Add;
+
+public class Add {
+	public int Add(int a, int b)
+	{
+		return a+b;
+	}
+}
