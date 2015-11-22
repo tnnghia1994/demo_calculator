94b8e0c3b95f12a3a64bbd60fff89a23d4d1a95d
tnnghia1994 <tnnghia1994@gmail.com>
2015-11-22 12:47:06 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String arg[])
 	{
 		Add add = new Add();
-		int result = add.Add(10,20);
+		int result = add.Add(10,20)
 		System.out.println(result);
 	}
 	public boolean test()
