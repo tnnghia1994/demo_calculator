d26b81cf5451cb2b47d2ff62ce6ec7adc4f525fd
Vũ Thanh Sang <sangthanh980@gmail.com>
2015-11-22 10:06:29 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,7 @@ public class Main {
 	public static void main(String arg[])
 	{
 		Add add = new Add();
-		int result = add.Ad
-		System.out.println(result)
-	}
-}
-public class Test {
-	public boolean test()
-	{
-		return add.Add(10,20) == 15;
+		int result = add.Add(10,20);
+		System.out.println(result);
 	}
 }
